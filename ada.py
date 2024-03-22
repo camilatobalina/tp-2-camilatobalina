4e12a019319fe9b7a60747f6f29138793a748e63
camilatobalina <162723051+camilatobalina@users.noreply.github.com>
2024-03-22 12:39:20 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,7 +1,7 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
-    full_name= first_name.lower() + " " + last_name.lower()
+    full_name= f"{first_name.lower()} {last_name.lower()}"
     print(full_name)
     print(full_name.title())
     print(full_name.upper())
